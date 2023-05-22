4404db5182c0acb113c36a088cd6bc8bc019cd9d
cynthiajweaverdev <117557857+cynthiaweaver@users.noreply.github.com>
2023-05-22 13:26:25 -0400

    Revert "setup"
    This reverts commit a9ef76b931e8aadb05c8489a18e99d2681dc5625.

--- src/products/products.controller.js
+++ src/products/products.controller.js
@@ -1,26 +1,53 @@
 const productsService = require("./products.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
-async function productExists(req, res, next) {
+
+//example of .then()
+// function productExists(req, res, next) {
+//   productsService
+//     .read(req.params.productId)
+//     .then((product) => {
+//       if (product) {
+//         res.locals.product = product;
+//         return next();
+//       }
+//       next({ status: 404, message: `Product cannot be found.` });
+//     })
+//     .catch(next);
+// }
+
+//async/await
+async function productExists (req, res, next) {
   const product = await productsService.read(req.params.productId);
-  if (product) {
+  if(product) {
     res.locals.product = product;
-    return next();
+    return next();    
   }
-  next({ status: 404, message: `Product cannot be found.` });
+  next({
+    status: 404,
+    message:  `Product cannot be found.` 
+  });
 }
+
 
 function read(req, res) {
   const { product: data } = res.locals;
   res.json({ data });
 }
 
-async function list(req, res) {
+// function list(req, res, next) {
+//   productsService
+//     .list()
+//     .then((data) => res.json({ data }))
+//     .catch(next);
+// }
+
+//async example
+async function list(req, res, next) {
   const data = await productsService.list();
   res.json({ data });
 }
-
 module.exports = {
-  read: [asyncErrorBoundary(productExists), asyncErrorBoundary(read)],
+  read: [asyncErrorBoundary(productExists), read],
   list: asyncErrorBoundary(list),
 };
